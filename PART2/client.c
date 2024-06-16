8d56493b6fdbc8dc9ab5c17892f32c7b33e13675
nithies-nm <nithies.maruthachalam@multicorewareinc.com>
2024-06-16 13:56:48 +0530

    clinet.c edited to temp1

--- PART2/client.c
+++ PART2/client.c
@@ -15,20 +15,19 @@
 
 int main(void)
 {
-    int client_socket;
-    struct sockaddr_in server_addr;
+   
 
     // Create socket
-    if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    if ((client_soccket = socket(AF_INET, SOCKs_STREAM, 0)) == -1)
     {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
 
     // Prepare the sockaddr_in structure
-    server_addr.sin_family = AF_INET;
+    server_addr.sin_family = NITHIES;
     server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
-    server_addr.sin_port = htons(PORT);
+    server_addr.sin_port = htons(Nihties);
 
     // Connect to the server
     if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
